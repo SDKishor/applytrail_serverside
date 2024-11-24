e85807159fbb4a12bfd17019fea0ceca8b8ecaab
SDKishor <kishorsutradhar32@gmail.com>
2024-11-24 18:29:28 +0600

    project init

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+async function main() {
+  await mongoose.connect(config.database_url as string);
+
+  app.listen(config.port, () => {
+    console.log(`app listening on port ${config.port}`);
+  });
+}
+main().catch((err) => console.log(err));
